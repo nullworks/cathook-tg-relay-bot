9db963fce791de5c77bf57db2bf9c56e30e1e1d9
Bintr <20655963+explowz@users.noreply.github.com>
2022-11-07 16:22:27 +0200

    Small fixes * Reformat according to JavaScript Standard Style * Attempt to add comments to make the code easier to understand * Removed unused variables * Don't allow user to run the script without setting up their config.json

--- README.md
+++ README.md
@@ -1,31 +1,33 @@
+[![JavaScript Style Guide](https://cdn.rawgit.com/standard/standard/master/badge.svg)](https://github.com/standard/standard)
 # cathook-tg-relay-bot
-Relay bot for catbots on Telegram
+Telegram relay bot for catbots
 
-# How do i Set up this Bot?
+# How do I set up this bot?
 - Download this repository and `cd` inside of it
 - Run `npm i` and install the dependencies
 - Get a Telegram bot using the [BotFather](https://t.me/BotFather)
-- Retrieve the auth token and save it in `config.json` by replacing `Your Telegram bot token here` With the appropiate token.
-- Create a Channel/Group and add Your bot to it (See below on how to add the Bot)
+- Retrieve the auth token and save it in `config.json` by replacing `Your Telegram bot token here` With the appropriate token.
+- Create a channel/group and add your bot to it (see below on how to add the bot)
+
 ![](https://i.imgur.com/scinN4O.png)
 
 ![](https://i.imgur.com/LVQgGSo.png)
 
 ![](https://i.imgur.com/lo82yxM.png)
 
-Obviously, Search YOUR bot here, not ours
+Obviously, search YOUR bot here, not ours
 
 ![](https://i.imgur.com/BRclXtO.png)
 
 ![](https://i.imgur.com/yb2iB3p.png)
-- You will now need to get your channel ID,to do this send a message to your channel, and Forward it to the [GetIDs Bot](https://t.me/getidsbot)
+- You will now need to get your channel ID; to do so send a message in your channel, and forward it to [GetIDs Bot](https://t.me/getidsbot)
 - Copy the "Origin chat" id field (including the minus, see below)
 
 ![](https://i.imgur.com/VI5t4IF.png)
-- Put the ID in `config.json` by replacing `Your Channel ID here` with the channel ID.
+- Put the id in `config.json` by replacing `Your Channel ID here` with the channel ID.
 - You are now done with setting the bot up
 
-# How do i use this bot?
-- After your catbots initialized the `.csv` files in `/opt/cathook/data` after their first few ingame moments, Run the bot by typing `node bot.js`
-- The bot will now automatically log every Chat message received to your relay channel.
+# How do I use this bot?
+- Once your catbots have initialized the `.csv` files in `/opt/cathook/data` after their first few in-game moments, run the bot by typing `node bot.js`
+- The bot will now automatically log every chat message received to your relay channel.
 - Enjoy the salt :)
--- bot.js
+++ bot.js
@@ -1,176 +1,166 @@
-const Tail = require('tail').Tail;
-const dateformat = require('dateformat');
-const fs = require('fs');
-const requestpromise = require("request-promise-native");
-const token = require('./config.json').token;
-const channel = require('./config.json').channel;
+'use strict'
+const Tail = require('tail').Tail
+const fs = require('fs')
+const requestPromise = require('request-promise-native')
+const token = require('./config.json').token
+if (token === 'Your Telegram bot token here') throw new Error('Set your bot token in config.json!')
+const channel = require('./config.json').channel
+if (channel === 'Your Channel ID here') throw new Error('Set your channel id in config.json!')
 
-const queue = [];
-const stack = [];
-
-const stack_size = 10;
-let stack_iterator = 0;
-
-function splitCSV(csv) {
-    let result = [];
-    let current = '';
-    let quotes = false;
-    for (let i = 0; i < csv.length; ++i) {
-        if (csv[i] === '"' && csv[i + 1] === '"') {
-            current += '"';
-            ++i;
-            continue;
-        }
-        if (csv[i] === '"') {
-            quotes = !quotes;
-            continue;
-        }
-        if (csv[i] === ',' && !quotes) {
-            result.push(current);
-            current = '';
-            continue;
-        }
-        current += csv[i];
+const splitCSV = (csv) => {
+  const result = []
+  let current = ''
+  let quotes = false
+  for (let i = 0; i < csv.length; ++i) {
+    if (csv[i] === '"' && csv[i + 1] === '"') {
+      current += '"'
+      ++i
+      continue
     }
-    result.push(current);
-    return result;
-}
-
-let antiSpamData = {};
-let muted = {};
-
-function antiSpam(data) {
-    let steamID = data[1];
-    if (muted[steamID])
-        return false;
-    if (!antiSpamData[steamID])
-        antiSpamData[steamID] = {count: 0, last: 0};
-    if (Date.now() - antiSpamData[steamID].last > 15 * 1000)
-        antiSpamData[steamID].count = 0;
-    antiSpamData[steamID].count++;
-    antiSpamData[steamID].last = Date.now();
-    if (antiSpamData[steamID].count > 8) {
-        //console.log(`Banning ${steamID} for spamming`);
-        antiSpamData[steamID].banned = Date.now();
+    if (csv[i] === '"') {
+      quotes = !quotes
+      continue
     }
-    return !antiSpamData[steamID].banned;
-}
-
-function composeMessage(data) {
-    let time = dateformat(+data[0] * 1000, "HH:MM:ss");
-    let steamID = data[1];
-    let username = data[2];
-    let message = data[3];
-    let ipcID = data[4];
-
-    return `[ID ${ipcID}] [${time}] [U:1:${steamID}] ${username}: ${message}`;
-}
-
-function composeMessageRaw(data) {
-    let time = dateformat(+data[0] * 1000, "HH:MM:ss");
-    let steamID = data[1];
-    let username = data[2];
-    let message = data[3];
-    let ipcID = data[4];
-
-    return `[ID ${ipcID}] [${time}] [U:1:${steamID}] ${username}: ${message}`;
-}
-
-function getSpamCheckData(data) {
-    let time = dateformat(+data[0] * 1000, "HH:MM:ss");
-    let steamID = data[1];
-    let username = data[2];
-    let message = data[3];
-    //let ipcID = data[4];
-    return `[${time}] [U:1:${steamID}] ${username}: ${message}`;
-}
-
-function test_and_set(msg) {
-    let j = stack_iterator;
-    for (let i = 0; i < 10; i++) {
-        if (stack[j] === msg) return false;
-        j++;
-        if (j >= stack_size) j = 0;
+    if (csv[i] === ',' && !quotes) {
+      result.push(current)
+      current = ''
+      continue
     }
-    stack[stack_iterator++] = msg;
-    if (stack_iterator >= stack_size) stack_iterator = 0;
-    return true;
+    current += csv[i]
+  }
+  result.push(current)
+  return result
 }
 
-function onLine(data) {
+const antiSpamData = {}
+const muted = {}
+const antiSpam = (data) => {
+  const steamID = data[1]
+  if (muted[steamID]) return false
+  if (!antiSpamData[steamID]) antiSpamData[steamID] = { count: 0, last: 0 }
+  if (Date.now() - antiSpamData[steamID].last > 15000) antiSpamData[steamID].count = 0
+  antiSpamData[steamID].count++
+  antiSpamData[steamID].last = Date.now()
+  if (antiSpamData[steamID].count > 8) antiSpamData[steamID].banned = Date.now()
+  return !antiSpamData[steamID].banned
+}
+
+const composeMessage = (data) => {
+  const steamID = data[1]
+  const username = data[2]
+  const message = data[3]
+  const ipcID = data[4]
+
+  return `[ID ${ipcID}] [U:1:${steamID}] ${username}: ${message}`
+}
+
+const composeMessageRaw = (data) => {
+  const steamID = data[1]
+  const username = data[2]
+  const message = data[3]
+  const ipcID = data[4]
+
+  return `[ID ${ipcID}] [U:1:${steamID}] ${username}: ${message}`
+}
+
+const getSpamCheckData = (data) => {
+  const steamID = data[1]
+  const username = data[2]
+  const message = data[3]
+
+  return `[U:1:${steamID}] ${username}: ${message}`
+}
+
+const stack = []
+const stackSize = 10
+let stackIterator = 0
+const testAndSet = (msg) => {
+  let j = stackIterator
+  for (let i = 0; i < 10; i++) {
+    if (stack[j] === msg) return false
+    j++
+    if (j >= stackSize) j = 0
+  }
+  stack[stackIterator++] = msg
+  if (stackIterator >= stackSize) stackIterator = 0
+  return true
+}
+
+const queue = []
+const onLine = (data) => {
+  try {
+    if (testAndSet(data)) {
+      queue.push(data)
+    }
+  } catch (e) {
+  }
+}
+
+const send = () => {
+  try {
+    let msg = ''
+    let msgRaw = ''
+    // No new messages
+    if (!queue.length) return
+    while (queue.length) {
+      try {
+        const csv = queue.shift()
+        const data = splitCSV(csv)
+        if (!antiSpam(data)) continue
+        const message = composeMessage(data)
+        const spamCheck = getSpamCheckData(data)
+        if (!msg.contains(spamCheck)) continue
+
+        msg += `${message}\n`
+        msgRaw += `${composeMessageRaw(data)}\n`
+      } catch (e) {
+        console.log(`ERROR: ${e}`)
+      }
+    }
+    if (msgRaw === '') return
     try {
-        if (test_and_set(data)) {
-            queue.push(data);
-        }
+      process.stdout.write(msgRaw)
+      requestPromise(
+        `https://api.telegram.org/bot${token}/sendMessage?chat_id=${channel}&disable_web_page_preview=True&text=${encodeURIComponent(msg)}`)
     } catch (e) {
     }
+  } catch (e) {
+  }
 }
 
+setInterval(send, 8000)
 
-function send() {
-    try {
-        let msg = '​​';
-        let msgRaw = '';
-        if (!queue.length) return;
-        while (queue.length) {
-            try {
-                let csv = queue.shift();
-                let data = splitCSV(csv);
-                if (!antiSpam(data))
-                    continue;
-                let message = composeMessage(data);
-                let spam_check = getSpamCheckData(data);
-                if (msg.indexOf(spam_check) !== -1)
-                    continue;
+const onError = (error) => {
+  console.log(`ERROR: ${error}`)
+}
 
-                msg += message + '\n'
-                msgRaw += composeMessageRaw(data) + '\n';
-            } catch (e) {
-                console.log('error', e);
-            }
+// Used as a list to know which files we're watching
+const watching = {}
+const locateLogs = () => {
+  try {
+    // Function to get current filenames in directory
+    fs.readdir('/opt/cathook/data', (error, files) => {
+      if (error) {
+        console.log(error)
+        return
+      }
+      for (let file of files) {
+        // If there's no listener installed, and the file's name is chat-*.cvs, install a listener on it
+        if (!watching[file] && /chat-.+\.csv/.exec(file)) {
+          console.log(`Found log file: ${file}`)
+          file = `/opt/cathook/data/${file}`
+          const tail = new Tail(file)
+          tail.on('line', onLine)
+          tail.on('error', onError)
+          // Add the file to the watching list, so that we don't accidentally install a new listener on it
+          watching[file] = true
         }
-        if (msgRaw === '') return;
-        try {
-            process.stdout.write(msgRaw);
-            requestpromise(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${channel}&disable_web_page_preview=True&text=` + encodeURIComponent(msg));
-        } catch (e) {
-        }
-    } catch (e) {
-    }
+      }
+    })
+  } catch (e) {
+    onError(e)
+  }
 }
 
-setInterval(send, 8000);
-
-function onError(error) {
-    console.log('ERROR:', error);
-}
-
-let watching = {};
-const tails = [];
-
-function locateLogs() {
-    try {
-        fs.readdir('/opt/cathook/data', (error, files) => {
-            if (error) {
-                console.log(error);
-                return;
-            }
-            for (let file of files) {
-                file = '/opt/cathook/data/' + file;
-                if (!watching[file] && /chat-.+\.csv/.exec(file)) {
-                    console.log(`Found log file: ${file}`);
-                    let tail = new Tail(file);
-                    tail.on('line', onLine);
-                    tail.on('error', onError);
-                    tails.push(tail);
-                    watching[file] = true;
-                }
-            }
-        });
-    } catch (e) {
-        onError(e);
-    }
-}
-
-locateLogs();
-setInterval(locateLogs, 20000);
+locateLogs()
+setInterval(locateLogs, 20000)
